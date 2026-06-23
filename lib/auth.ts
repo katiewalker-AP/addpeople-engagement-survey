6a9132a0838356bfd2ecfdc3d37ddac5f149bf5b
Katie Walker <katiewalker@ADD-1647.ico.pri>
2026-06-23 13:49:11 +0100

    add thrivemediagroup emails to allowed list

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,10 +2,13 @@ import type { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const ALLOWED_EMAILS = [
-  'kerry.matthewman@thrivemediagroup.co.uk',
   'katie.walker@addpeople.co.uk',
+  'katie.walker@thrivemediagroup.co.uk',
+  'kerry.matthewman@thrivemediagroup.co.uk',
   'chloe.potts@addpeople.co.uk',
+  'chloe.potts@thrivemediagroup.co.uk',
   'richard.poskitt@addpeople.co.uk',
+  'richard.poskitt@thrivemediagroup.co.uk',
 ];
 
 export const authOptions: NextAuthOptions = {
